35c976bf6c068eab83f15c8361c1f9ba1dcec4a5
SMuyesser <smuyesser@gmail.com>
2017-07-26 15:08:15 -0700

    shallow tests added

---
+++ src/components/setup.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Setup from './Setup';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Setup />, div);
+});
