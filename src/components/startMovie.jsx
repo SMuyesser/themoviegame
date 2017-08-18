e8213e39c1c12fb7a49d0af3894dc46240cb5bec
SMuyesser <smuyesser@gmail.com>
2017-08-17 20:47:08 -0700

    added get request to back end

--- src/components/startMovie.jsx
+++ src/components/startMovie.jsx
@@ -20,7 +20,7 @@ export class StartMovie extends React.Component {
 		const value = event.target.value;
 		const component = this;
 		if (value.length > 3) {
-			axios.get('https://api.themoviedb.org/3/search/movie?api_key=7e9a1ff04b7576b3330211792aa796b5&language=en-US&query='+value+'&page=1&include_adult=false')
+			axios.get('http://localhost:8080/movieoptions/'+value)
 			.then((response) => {
 				component.setState({
 					startOptions: response.data.results
