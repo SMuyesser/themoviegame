cb27f35dc5e90f3793544362c418dd7faa14e7b1
SMuyesser <smuyesser@gmail.com>
2017-08-20 15:29:31 -0700

    updated config

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   // other stuff
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "https://localhost:8080"
+  	'https://intense-bayou-56203.herokuapp.com' ||
+    "http://localhost:8080"
 };
